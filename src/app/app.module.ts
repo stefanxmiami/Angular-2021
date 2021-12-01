90a7e0ca37c2ff5de16b10a9debebc17b1dd35e6
Stefan Ivanov <stefan.miami99@gmail.com>
2021-12-01 15:54:16 +0100

    rare  upload by me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { eventcardComponent } from 'src/_eventcard/eventcard.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent, eventcardComponent],
-  imports: [ BrowserModule, AppRoutingModule],
+  imports: [ BrowserModule, AppRoutingModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent]
 })
