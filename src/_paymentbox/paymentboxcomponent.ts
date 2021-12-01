50dfe635fb83562db1bec41baee6d1753a971b2a
David <95292339+DavidDonev@users.noreply.github.com>
2021-12-01 19:38:50 +0100

    payment i dr promeni

--- src/_paymentbox/paymentboxcomponent.ts
+++ src/_paymentbox/paymentboxcomponent.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
- selector: '',
- templateUrl: ''
+ selector: 'pm-payment',
+ templateUrl: './paymentboxcomponent.html'
 })
 
 export class paymentboxComponent {
-
+  
 }
