b4df1717aa5cbb4dfcc5b28695f95dddc31d7f7a
LevkoJ <95293075+LevkoJ@users.noreply.github.com>
2021-12-01 12:34:33 +0100

    router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  directives: [ROUTER_DIRECTIVES]
 })
 export class AppComponent {
 
