f94ef354ef010e1b482ad35a2bdd32b68c61b54c
David <95292339+DavidDonev@users.noreply.github.com>
2021-12-01 21:41:02 +0100

    .

--- src/app/_eventcard/eventcard.component.ts
+++ src/app/_eventcard/eventcard.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
  selector: 'eventcard',
- templateUrl: './eventcard.component.html'
+ templateUrl: '../eventcard.component.html'
 })
 
 export class eventcardComponent {
