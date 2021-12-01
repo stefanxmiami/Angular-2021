8a221db0f2bcf320a26238a2f8ca0e333d1d2ef2
David <95292339+DavidDonev@users.noreply.github.com>
2021-12-01 21:33:31 +0100

    probapayment

--- src/app/_paymentbox/paymentboxcomponent.ts
+++ src/app/_paymentbox/paymentboxcomponent.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
  selector: 'pm-payment',
- templateUrl: './paymentboxcomponent.html'
+ templateUrl: './paymentboxcomponent.html',
+ styleUrls: ['stefanxmiami/Angular-2021/src/app/_paymentbox/paymentboxcomponent.css'],
 })
 
 export class paymentboxComponent {
