f372736fce4dc9d2c429855e0889addaff5bb957
Stefan Ivanov <stefan.miami99@gmail.com>
2021-12-01 13:46:40 +0100

    enter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { eventcardComponent } from 'src/_eventcard/eventcard.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, eventcardComponent],
   imports: [ BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
