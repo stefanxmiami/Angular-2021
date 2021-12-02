954703d9c2b155960a4c060fe52a216d8e54b0b9
Stefan Ivanov <stefan.miami99@gmail.com>
2021-12-02 15:30:03 +0100

    kjh

--- src/app/_eventcard/eventcard.component.ts
+++ src/app/_eventcard/eventcard.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
  selector: 'eventcard',
- templateUrl: './_eventcard/eventcard.component'
+ templateUrl: '../_eventcard/eventcard.component.html'
 })
 
 export class eventcardComponent {
