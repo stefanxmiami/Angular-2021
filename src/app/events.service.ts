65df018aa571f99e6d3bcc2b6ce63d0a43ea5cfe
LevkoJ <95293075+LevkoJ@users.noreply.github.com>
2021-12-03 14:47:05 +0100

    .

--- src/app/events.service.ts
+++ src/app/events.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class EventsService {
   events = [
-    { name: 'Blue Ocean', id: 1, shortDescription: 'Your gift to Blue Ocean Society for Marine Conservation helps protect marine life in the Gulf of Maine and beyond.', picture: "../../assets/oPolution.jpg" },
+    { name: 'Blue Ocean', id: 1, shortDescription: 'Your gift to Blue Ocean Society for Marine Conservation helps protect marine life in the Gulf of Maine and beyond.'},
     { name: 'Air Polution', id: 2, shortDescription: 'Please donate to an award-winning charity fighting air pollution. We need all the help we can get to fight gasoline and the huge impact it has on airs.' },
     { name: 'ShareTheMeal', id: 3, shortDescription: '1 in 9 children goes to bed hungry every night. Start giving with ShareTheMeal. Be part of the generation that ends hunger.' },
   ];
